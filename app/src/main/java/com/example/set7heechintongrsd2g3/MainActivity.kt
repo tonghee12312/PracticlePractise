397f143992896d9347c531eca839f20453a5feb9
Forge-15 <Forge-15@DESKTOP-USIT37L>
2022-10-09 16:12:58 +0800

    update reset Button

--- app/src/main/java/com/example/set7heechintongrsd2g3/MainActivity.kt
+++ app/src/main/java/com/example/set7heechintongrsd2g3/MainActivity.kt
@@ -18,8 +18,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        reset()
+
         checkPermissions()
 
+        binding.buttonReset.setOnClickListener {
+            reset()
+        }
+        binding.buttonCalculate.setOnClickListener{
+            val name = binding.editTextName.toString()
+            val age = getAge()
+            val Year = getYear()
+        }
         binding.buttonphonecall.setOnClickListener {
             val phone = Intent(
                 Intent.ACTION_CALL,
@@ -37,11 +47,33 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+    private fun reset() {
+        binding.editTextName.text.clear()
+        binding.radioGroupAge.check(R.id.radioButtonUnderAge)
+        binding.spinnertaxIncome.setSelection(0)
+        binding.radioGroupYear.check(R.id.radioButtonYear2015)
+    }
+
+    private fun getYear(): String {
+        return when(binding.radioGroupYear.checkedRadioButtonId){
+            R.id.radioButtonYear2015 -> getString(R.string._2015)
+            R.id.radioButtonyear2016 -> getString(R.string._2016)
+            else ->""
+        }
+
+    }
+
     private fun checkPermissions() {
         if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CALL_PHONE) !=PackageManager.PERMISSION_GRANTED){
         ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CALL_PHONE), 101)
         }
+        }
+    private fun getAge(): String {
+        return when(binding.radioGroupAge.checkedRadioButtonId) {
+            R.id.radioButtonUnderAge -> getString(R.string.aboveAge)
+            R.id.radioButtonAboveAge -> getString(R.string.belowAge)
+            else -> ""
+        }
     }
-
 
 }
