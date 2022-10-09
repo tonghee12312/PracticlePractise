d8b593568365dad77c6e0d313fbb00c9a27a1c82
Forge-15 <Forge-15@DESKTOP-USIT37L>
2022-10-09 15:35:12 +0800

    update website and phone call

--- app/src/main/java/com/example/set7heechintongrsd2g3/MainActivity.kt
+++ app/src/main/java/com/example/set7heechintongrsd2g3/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.set7heechintongrsd2g3
 
+import android.content.ClipData.newIntent
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Layout
-import android.view.LayoutInflater
-import android.widget.ArrayAdapter
+import androidx.core.app.ActivityCompat
+
 import com.example.set7heechintongrsd2g3.databinding.ActivityMainBinding
+import java.util.jar.Manifest
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -13,7 +18,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        checkPermissions()
 
+        binding.buttonphonecall.setOnClickListener {
+            val phone = Intent(
+                Intent.ACTION_CALL,
+                Uri.parse("tel:0383138888")
+            )
+            startActivity(phone)
+        }
+            binding.buttonWebsite.setOnClickListener {
+                val web = Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://www.hasil.gov.my/")
+                )
+                startActivity(web)
+            }
 
+        }
+
+    private fun checkPermissions() {
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CALL_PHONE) !=PackageManager.PERMISSION_GRANTED){
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CALL_PHONE), 101)
+        }
     }
+
+
 }
